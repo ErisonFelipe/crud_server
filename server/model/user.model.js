e6c0ba39380344bb0653b639901b7c1d6eed6f69
Erison Felipe <erison274@gmail.com>
2021-08-31 22:21:06 -0300

    Banco-de-Dados

---
+++ server/model/user.model.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../config/db.config');
+
+const User = sequelize.define('User', {
+  // Model attributes are defined here
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING
+    // allowNull defaults to true
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  dateNascimento: {
+    type: Sequelize.DATE
+  },
+  phone: {
+    type: Sequelize.INTEGER
+  }
+}, {
+  // Other model options go here
+});
+
+// User.sync({ force: true });
