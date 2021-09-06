20153d981fcf6179aa99cc6c7dbfa5a2ed50f204
Erison Felipe <erison274@gmail.com>
2021-09-05 21:04:22 -0300

    middleware/cors

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const server = express();
 
 var UserRouter = require('./routes/user.router');
 var LoginRouter = require('./routes/login.router');
 var LogMiddleware = require('./middleware/log');
 
+server.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    server.use(cors());
+    next();
+});
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(LogMiddleware);
+
 
 server.use(UserRouter);
 server.use(LoginRouter);
